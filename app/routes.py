aee17958f0cca7cf5de41cc41b4b877db1dc0f91
Daniel Buselli <buselli.daniel@gmail.com>
2024-04-07 19:02:52 +0300

    User handling done

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,47 @@
 from app import app
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, flash, session
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        if users.login(request.form["username"], request.form["password"]):
+            return redirect("/dashboard")
+        else:
+            flash("Incorrect username or password")
+            return render_template("login.html")
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    users.logout()
+    return render_template("index.html")
 
 @app.route("/dashboard")
 def dashboard():
-    sql = "SELECT id, content, created_at FROM polls ORDER BY id DESC"
-    result = db.session.execute(sql)
-    posts = result.fetchall()
-    return render_template("dashboard.html", items=posts)
+    testitems = ["moi", "testi", "jee"]
+    return render_template("dashboard.html", items=testitems)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    error = None
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            error = "Mismatching passwords"
+            return render_template("error.html", error=error)
+        if users.register(username, password1):
+            flash('Account succesfully created')
+            return redirect("/login")
+        else:
+            error = "Registration failed"
+            return render_template("error.html", error=error)
--- app/users.py
+++ app/users.py
@@ -1,9 +1,10 @@
 from db import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.sql import text
 
 def login(username, password):
-    sql = "SELECT id, password FROM users WHERE username=:username"
+    sql = text("SELECT id, password FROM users WHERE username=:username")
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
@@ -21,10 +22,11 @@ def logout():
 def register(username, password):
     hash_value = generate_password_hash(password)
     try:
-        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+        sql = text("INSERT INTO users (username,password) VALUES (:username,:password)")
         db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
     except:
+        print(False)
         return False
     return login(username, password)
 
